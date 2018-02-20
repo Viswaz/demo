9904d2da0fb1036449b544df57cbe44379987147
viswa <viswaz@gmail.com>
2018-02-20 12:34:05 -0600

    added principle attributes

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -1,18 +1,22 @@
 package com.example.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.Map;
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
-	private final AtomicLong counter = new AtomicLong();
 	
 	@RequestMapping(value="/")
-	public String home() {
-		return "Hello World !!! " + counter.getAndIncrement();
+	public String home(OAuth2AuthenticationToken authentication) {
+		String result="";
+		Map<String, Object> attrs = authentication.getPrincipal().getAttributes(); 
+		for (Map.Entry<String, Object> entry : attrs.entrySet()) {
+		    result += (entry.getKey() + " = " + entry.getValue() + "<br>");
+		}
+		return "Hello User !!! <br> Here are your details: <br>" + result;
 	}
 	
 }
